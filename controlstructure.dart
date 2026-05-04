dabeafd6e6bdc53ea4d4c7ffbb2d84b63c41fbd6
Hajur Karki <karkihajur01@gmail.com>
2026-05-04 12:03:56 +0545

    update

--- controlstructure.dart
+++ controlstructure.dart
@@ -4,11 +4,42 @@
 /// switch case structure
 
 
-main(){
-  var name='hajur';
-  if(name=='hajur')
-  {
-    print('rightname');
-  }
+///if structure
 
+// main(){
+//   var name='hajur';
+//   if(name=='hajur')
+//   {
+//     print('rightname1');
+//   } 
+//   else if(name=='karki') {
+//     print(' rightname2');
+//   }
+//   else{
+//     print('wrong name');
+//   }
+
+// }
+
+
+/// switch case structure
+main(){
+  var name='hey';
+
+  switch(name){
+    case 'hajur':
+    print('rightname1');
+    break;
+
+    case 'karki':
+    print('rightname2');
+    break;
+
+
+    default:
+    print('wrong name');
+
+
+  }
+  
 }
