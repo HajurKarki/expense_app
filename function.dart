f0d91a03f175d92949df51475e6b5cb09703ce6a
Hajur Karki <karkihajur01@gmail.com>
2026-05-04 13:17:18 +0545

    update

---
+++ function.dart
@@ -0,0 +1,59 @@
+/// FUNCTION BASIC RULES
+/// FUNCTION EXPRESSION - FAT arrpw
+/// parameter
+/// optional parameter
+/// named paramater
+/// 
+
+
+// main(){
+//  getNumber(10,20);
+// }
+
+// getNumber(int x, int b){
+//   print(x*b);
+//   print(x);
+//   print(b);
+// }
+
+
+  
+  // main(){
+  //   var  x=getNumber(10,20);
+  //   print(x*10);
+
+  // }
+  // getNumber(int x, int b){
+  //   return x*b;
+  // }
+
+
+// void main() {
+//   var  x=getNumber(10,20);
+//   print(x*2);
+  
+// }
+// getNumber(int x, int b){
+//   var y = (x*b)+10;
+//   return y;
+// }
+
+///OR
+/// function expression - fat arrow function
+// main(){
+//   var x = getNumber(10,20);
+//   print(x*2);
+
+// }
+// getNumber(int x, int b) =>   (x*b)+10; // this is the fat arrow function which is used to return a value in a single line of code. it is also called as expression function. it is a shorthand for the function that returns a value. it is also called as lambda function. it is also called as anonymous function. it is also called as closure. it is also called as higher order function. it is also called as first class function. it is also called as arrow function. it is also called as fat arrow function. it is also called as expression function. it is also called as lambda function. it is also called as anonymous function. it is also called as closure. it is also called as higher order function. it is also called as first class function.
+
+
+/// optiomal parameter
+/// 
+ main(){
+  getNumber(10,20);
+
+ }
+ getNumber(int x, [int b=10]){ // this is the optional parameter which is used to make the parameter optional. it is also called as positional parameter. it is also called as optional positional parameter. it is also called as optional named parameter. it is also called as named parameter. it is also called as optional parameter. it is also called as positional parameter. it is also called as optional positional parameter. it is also called as optional named parameter. it is also called as named parameter.
+   print(x*b);
+ }
---
+++ loop.dart
@@ -0,0 +1,35 @@
+/// for loop
+// main(){
+//   for(int i=1; i<=10; i++){
+//     if(i==5){
+//       break; // it will break the loop when i is 5
+//     }
+//     print(i);
+//   }
+// }
+
+
+// /// while loop
+//  main(){
+//   int i=1;
+//   while(i<=10){
+//     if(i==5){
+//       break; // it will break the loop when i is 5
+//     }
+    
+//     print(i);
+//     i++;
+//   }
+//  }
+
+
+/// do while loop
+/// 
+  main(){
+    int i=11;
+    do{
+      print(i);
+      i++;
+    } while(i<=10);
+  }
+
